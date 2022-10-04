c1e5b912758225b3db2ea5ed34b7d8a7beb8cdd6
Kucheriavenko Dmitrii <Kucherdimdimych@mail.ru>
2022-10-04 13:42:49 +0500

    add registration service, .gitignore

--- shop/app_user/urls.py
+++ shop/app_user/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from app_user.views import *
+
+
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
@@ -9,4 +11,10 @@ urlpatterns = [
     path('profile/<int:pk>/edit/', UpdateProfile.as_view(), name='profile_edit'),
     path('history_view/<int:pk>/', DetailHistoryView.as_view(), name='history_view'),
     path('profile_edit/<int:pk>/', UpdateProfile.as_view(), name='profile_edit'),
+
+    path("activate/<slug:uidb64>/<slug:token>/", account_activate, name="activate"),
+    path("activate/account_activated/", ActivatedAccount.as_view(), name="activated_success"),
+    path("activate/invalid_activation/", InvalidActivatedAccount.as_view(), name="invalid_activation"),
+
+
 ]
